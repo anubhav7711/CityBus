07251e4a76f94b819488391b757a0324a5a12cf8
Anubhav BansaL <itsanubhav120@gmailc.com>
2026-05-05 13:50:29 +0530

    origin-updated

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:3000",        // local dev
   "https://buscity.netlify.app",
-  "https://city-bus-neon.vercel.app/",  // your deployed frontend
+  "https://city-bus-neon.vercel.app",  // your deployed frontend
 ];
 
 app.use(cors({
